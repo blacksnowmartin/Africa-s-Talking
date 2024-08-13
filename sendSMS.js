a69323e35dfccbc935e3391d32e1dbdca0422687
blacksnowmartin <martinmwendwa005@gmail.com>
2024-08-13 05:28:41 +0300

    added a script

--- sendSMS.js
+++ sendSMS.js
@@ -1,1 +1,25 @@
+const AfricasTalking = require('africastalking');
 
+// TODO: Initialize Africa's Talking
+const africastalking = AfricasTalking({
+  apiKey: 'atsk_753a0d6514aebb6e4c622ef2db054ea25efda84c4431af26a057381e3132151c36d979b0', 
+  username: 'sandbox'
+});
+
+
+
+module.exports = async function sendSMS() {
+    
+    // TODO: Send message
+try {
+  const result=await africastalking.SMS.send({
+    to: '[+254700895391]', 
+    message: 'Hello Blacksnow Martin... Do not forget you will always remain great!',
+    from: '[37779]'
+  });
+  console.log(result);
+} catch(ex) {
+  console.error(ex);
+} 
+
+};
