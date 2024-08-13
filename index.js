e2e20be4d3da6fb081697518bef2a417f24cda8d
blacksnowmartin <martinmwendwa005@gmail.com>
2024-08-13 05:27:59 +0300

    added  index

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//const sendSMS = require('./sendSMS');
+const smsServer = require('./smsServer');
+const sendSMS = require('./sendSMS');
 
+sendSMS();
+
+// TODO: Call sendSMS function
+
+//sendSMS();
+
+// TODO: Call start sms server
+
+smsServer();
